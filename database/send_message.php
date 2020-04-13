08013f51bda54b84feb9e80b2f6716bb2870080c
sylwiazwol <48494412+sylwiazwol@users.noreply.github.com>
2020-04-13 20:17:09 +0200

    database

--- database/send_message.php
+++ database/send_message.php
@@ -1,22 +1,56 @@
-<?php
-		include_once("autoryzacja.php");
+ <?php
+//dane do logowania
 
-		$conn = mysql_connect($dbhost, $dbuser, $dbpass)
-		or  die('Bład polączenia z serwerem: '.mysql_error());
-		//echo "Polaczenie udane <br>";
-		mysql_select_db($dbname)
-		or die('Nie ma bazy o nazwie: '.$dbname);
+//Creating connection for mysqli
+include_once("autoryzacja.php");
+
+$conn = new mysqli($server, $user, $pass, $dbname);
+
+//Checking connection
+
+if ($conn->connect_error) {
+    die("Connection failed:" . $conn->connect_error);
+}
+
+if (isset($_POST ['submit'])) {
+
+    $first = $_POST['fname'];
+    $email = $_POST['email'];
+    $last = $_POST['lname'];
+    $message_text = $_POST['message_text'];
+
+    $sql = "INSERT INTO contact(firstname, lastname, email, message_text) VALUES ('$first', '$last', '$email', '$message_text')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "<h3> succcess </h3>";
+    } else {
+        echo "<h3> Failed </h3>";
+    }
+}
+$conn->close();
+?> 
+
+		<!-- // include_once("autoryzacja.php");
+
+		// $link = mysqli_connect("localhost", "17_zwolinska", "G7g4f6a1a6", "17_zwolinska");
+
+
+		// if (!$link) {
+		// 	echo "Error: Unable to connect to MySQL.";
+		// 	exit;
+		// }
 
 		
 		///////////////////////////////////////////////
 		//wykona polecenia, jeśli email nie będzie pusty
+// if(isset($wyslij)){
 
-
-		if($_POST['email']!=""){
-        mysql_query("INSERT INTO contact (email,firstname, lastname, message) VALUES ('".$_POST["email"]."','".$_POST["fname"]."','".$_POST["lname"]."', '".$_POST["message"]."')") or die ('Nie dodano informacji do bazy.');
-        $dateSend = date('Y-m-d');
-		mysql_query("INSERT INTO `contact` (`date_send`) VALUES ('$dateSend')");
-		
-		echo 'Wpisane dane przesłano do bazy<br>';
-		}
-		?>
+// 		if($_POST['email']!=""){
+//             mysqli->query("INSERT INTO contact (firstname, lastname, email, message_text) VALUES ('".$_POST["fname"]."','".$_POST["lname"]."','".$_POST["email"]."','".$_POST["message_text"]."')") or die ('Nie dodano  do bazy.');
+                
+//             echo 'Wpisane dane przesłano do bazy<br>';
+// 			}
+// 		else{
+// 			echo 'błąd';
+// 		}	
+// 		} -->
